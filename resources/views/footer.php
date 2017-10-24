c1d34f478bf9c9f9c6e637991bdc8881692fbb4e
Richmama <postmaster.richmama@gmail.com>
2017-10-24 22:18:39 +0100

    Change to blades

--- resources/views/footer.php
+++
@@ -1,9 +0,0 @@
-<footer class="footer">
-    <div class="container">
-        <div class="footer-logo"><a href="#"><img src="img/footer-logo.png" alt=""></a></div>
-        <span class="copyright">&copy; LottoUSA. All Rights Reserved</span>
-        <div class="credits">
-            <a href="#">Brought to you</a> by <a href="#">LottoUSA</a>
-        </div>
-    </div>
-</footer>
