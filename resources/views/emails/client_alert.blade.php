8724e32945c0850047fb3a463e11e6443aa54d4c
tkud04 <32499735+tkud04@users.noreply.github.com>
2017-10-27 20:18:34 +0100

    Anoda blade upload using ui

---
+++ resources/views/emails/client_alert.blade.php
@@ -0,0 +1,11 @@
+<center><h3>Your Client Just Applied For Lottery!</h3></center>
+<p>Hello <strong>admin</strong>, we are pleased to inform you that one of your clients just applied for lottery on our website. Please take note of the following details:</p><br>
+
+<center>
+<p>Client Name: <strong>{{$name}}<strong></p>
+<p>Client Phone number: <strong>{{$phone}}<strong></p>
+<p>Client Email address: <strong>{{$email}}<strong></p>
+</center><br>
+
+<h3>Next Steps</h3>
+<p style="color:#f00;">Make sure you follow up your client and most importantly GET PAID. Good luck!</p>
