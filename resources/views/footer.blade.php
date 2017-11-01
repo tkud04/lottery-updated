5a2d9546a9b090de59168eddd5b324b635b2a528
Richmama <postmaster.richmama@gmail.com>
2017-11-01 20:36:30 +0100

    Changed footer image

--- resources/views/footer.blade.php
+++ resources/views/footer.blade.php
@@ -1,9 +1,9 @@
 <footer class="footer">
     <div class="container">
-        <div class="footer-logo"><a href="#"><img src="img/footer-logo.png" alt=""></a></div>
-        <span class="copyright">&copy; LottoUSA. All Rights Reserved</span>
+        <div class="footer-logo"><a href="#"><img src="img/coat-of-arms.png" alt=""></a></div>
+        <span class="copyright">&copy; WorldLottoUSA. All Rights Reserved</span>
         <div class="credits">
-            <a href="#">Brought to you</a> by <a href="#">LottoUSA</a>
+            <a href="{{url('/')}}">Brought to you</a> by <a href="{{url('/')}}">WorldLottoUSA</a>
         </div>
     </div>
 </footer>
