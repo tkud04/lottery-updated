6981975993d550a962d92f605d12e50f27a190e7
Richmama <postmaster.richmama@gmail.com>
2017-11-01 13:46:10 +0100

    Made changes to services blade

--- resources/views/services.blade.php
+++ resources/views/services.blade.php
@@ -4,14 +4,15 @@
     	<h6>Apply today to stand a chance of winning $1 million!</h6>
         <div class="row">
         	<div class="col-lg-6 col-sm-6 wow fadeInLeft delay-05s">
-            	<div class="">
-                	<p>Thank you for your interest in this year's <strong>LottoUSA</strong>.</p><br>
-                	<p>Each year $50 million will be issued as lottery winnings to people from all over the world. The draw will take place through random computer generated lottery draw.</p><br>
-                    <p>Don't miss your chance to be a millionaire - apply online today and be one of the next lottery winners.</p><br>
+            	<div class="text-success">
+                	<h3>Thank you for your interest in this year's <strong>WorldLottoUSA</strong>.</h3><br>
+                	
+                    <h2>Don't miss your chance to be a millionaire - apply online today and be one of the next lottery winners.</h2><br>
                 </div>
             </div>
             <figure class="col-lg-6 col-sm-6  text-right wow fadeInUp delay-02s">
             	<img src="img/money-1.jpg" alt="">
+                 <h4>Each year $50 million will be issued as lottery winnings to people from all over the world. The draw will take place through random computer generated lottery draw.</h4><br>
             </figure>
         
         </div>
@@ -28,31 +29,31 @@
             </figure>
         	<div class="col-lg-7 col-sm-8 featured-work">
             	<h2>we pick players at random</h2>
-            	<P class="padding-b">We use a computer generated random system to randomly select lottery players based on their email address. Then we send them an email informing them of their selection and how much they stand to win if they apply.</P>
+            	<h3 class="padding-b">We use a computer generated random system to randomly select lottery players based on their <strong>email address</strong>.<br><br>Then we send them an email informing them of their selection and how much they stand to win if they apply.</h3><br>
             	<div class="featured-box">
                 	<div class="featured-box-col1 wow fadeInRight delay-02s">
-                    	<i class="fa-magic"></i>
+                    	<i class="fa-magic text-success"></i>
                     </div>	
                 	<div class="featured-box-col2 wow fadeInRight delay-02s">
-                        <h3>magic of changing lives</h3>
-                        <p>We give out these lottery wins to various people from around the world because we value changing people's lives</p>
+                        <h3 style="font-weight: bold;">magic of changing lives</h3>
+                        <p style="font-size: 80%; font-weight: bold;">We give out these lottery wins to various people from around the world because we value changing people's lives</p>
                     </div>    
                 </div>
                 <div class="featured-box">
                 	<div class="featured-box-col1 wow fadeInRight delay-04s">
-                    	<i class="fa-gift"></i>
+                    	<i class="fa-gift text-success"></i>
                     </div>	
                 	<div class="featured-box-col2 wow fadeInRight delay-04s">
-                        <h3>charitable cause</h3>
+                        <h3 style="font-weight: bold;">charitable cause</h3>
                         <p>We give out these lottery wins to various people from around the world because we value changing people's lives</p>
                     </div>    
                 </div>
                 <div class="featured-box">
                 	<div class="featured-box-col1 wow fadeInRight delay-06s">
-                    	<i class="fa-dashboard"></i>
+                    	<i class="fa-dashboard text-success"></i>
                     </div>	
                 	<div class="featured-box-col2 wow fadeInRight delay-06s">
-                        <h3>100% Support</h3>
+                        <h3 style="font-weight: bold;">100% Support</h3>
                         <p>Our agents will guide you from applying for the lottery to collecting your jackpot! </p>
                     </div>    
                 </div>
