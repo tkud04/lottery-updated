00d6b8175b8386c80f334fc64633bfe153a1038d
tkud04 <32499735+tkud04@users.noreply.github.com>
2017-11-02 21:27:58 +0100

    Uploaded layout and services_short

---
+++ resources/views/services_short.blade.php
@@ -0,0 +1,41 @@
+<section class="main-section alabaster"><!--main-section alabaster-start-->
+	<div class="container">
+    	<div class="row">
+			<figure class="col-lg-5 col-sm-4 wow fadeInLeft">
+            	<img  src="img/lottery-1.jpg" alt="">
+            </figure>
+        	<div class="col-lg-7 col-sm-8 featured-work">
+            	<h2>we pick players at random</h2>
+            	<h3 class="padding-b">We use a computer generated random system to randomly select lottery players based on their <strong>email address</strong>.<br><br>Then we send them an email informing them of their selection and how much they stand to win if they apply.</h3><br>
+            	<div class="featured-box">
+                	<div class="featured-box-col1 wow fadeInRight delay-02s">
+                    	<i class="fa-magic text-success"></i>
+                    </div>	
+                	<div class="featured-box-col2 wow fadeInRight delay-02s">
+                        <h3 style="font-weight: bold;">magic of changing lives</h3>
+                        <p style="font-size: 80%; font-weight: bold;">We give out these lottery wins to various people from around the world because we value changing people's lives</p>
+                    </div>    
+                </div>
+                <div class="featured-box">
+                	<div class="featured-box-col1 wow fadeInRight delay-04s">
+                    	<i class="fa-gift text-success"></i>
+                    </div>	
+                	<div class="featured-box-col2 wow fadeInRight delay-04s">
+                        <h3 style="font-weight: bold;">charitable cause</h3>
+                        <p>We give out these lottery wins to various people from around the world because we value changing people's lives</p>
+                    </div>    
+                </div>
+                <div class="featured-box">
+                	<div class="featured-box-col1 wow fadeInRight delay-06s">
+                    	<i class="fa-dashboard text-success"></i>
+                    </div>	
+                	<div class="featured-box-col2 wow fadeInRight delay-06s">
+                        <h3 style="font-weight: bold;">100% Support</h3>
+                        <p>Our agents will guide you from applying for the lottery to collecting your jackpot! </p>
+                    </div>    
+                </div>
+                <a class="Learn-More" href="#">Learn More</a>
+            </div>
+        </div>
+	</div>
+</section><!--main-section alabaster-end-->
