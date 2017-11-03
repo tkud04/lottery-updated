42d524c9e393e3f5ddeed402af57e98de1cce35c
tkud04 <32499735+tkud04@users.noreply.github.com>
2017-11-03 20:38:13 +0100

    Added Testimonials using web

---
+++ app/Testimonials.php
@@ -0,0 +1,14 @@
+<?php namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Testimonials extends Model {
+
+	/**
+	 * The attributes that are mass assignable.
+	 *
+	 * @var array
+	 */
+	protected $fillable = ['name', 'title', 'country', 'img', 'content', 'url'];
+
+}
