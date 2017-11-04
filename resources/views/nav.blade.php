0902302d4f4ca160787e36f2a6b4e95bfbaa3be6
Richmama <postmaster.richmama@gmail.com>
2017-11-04 10:29:18 +0100

    Added asset to nav flags

--- resources/views/nav.blade.php
+++ resources/views/nav.blade.php
@@ -11,40 +11,40 @@ div.shabba img{
 
 <div class="shabba">
 <center>
-<img src="img/flags/flag-of-United-States-of-America.png" alt="">
-<img src="img/flags/flag-of-United-Kingdom.png" alt="">
-<img src="img/flags/flag-of-Canada.png" alt="">
-<img src="img/flags/flag-of-Australia.png" alt="">
-<img src="img/flags/flag-of-Spain.png" alt="">
-<img src="img/flags/flag-of-Italy.png" alt="">
-<img src="img/flags/flag-of-Germany.png" alt="">
-<img src="img/flags/flag-of-France.png" alt="">
-<img src="img/flags/flag-of-Brazil.png" alt="">
-<img src="img/flags/flag-of-Venezuela.png" alt="">
-<img src="img/flags/flag-of-Sweden.png" alt="">
-<img src="img/flags/flag-of-Turkey.png" alt="">
-<img src="img/flags/flag-of-Russia.png" alt="">
-<img src="img/flags/flag-of-Portugal.png" alt="">
-<img src="img/flags/flag-of-New-Zealand.png" alt="">
-<img src="img/flags/flag-of-Netherlands.png" alt="">
-<img src="img/flags/flag-of-Malaysia.png" alt="">
-<img src="img/flags/flag-of-Liechtenstein.png" alt="">
-<img src="img/flags/flag-of-Lebanon.png" alt="">
-<img src="img/flags/flag-of-Japan.png" alt="">
-<img src="img/flags/flag-of-Jamaica.png" alt="">
-<img src="img/flags/flag-of-Ireland.png" alt="">
-<img src="img/flags/flag-of-India.png" alt="">
-<img src="img/flags/flag-of-Iceland.png" alt="">
-<img src="img/flags/flag-of-Hungary.png" alt="">
-<img src="img/flags/flag-of-Ghana.png" alt="">
-<img src="img/flags/flag-of-Greece.png" alt="">
-<img src="img/flags/flag-of-Cote-d-Ivoire.png" alt="">
-<img src="img/flags/flag-of-Estonia.png" alt="">
-<img src="img/flags/flag-of-Ecuador.png" alt="">
-<img src="img/flags/flag-of-Czech-Republic.png" alt="">
-<img src="img/flags/flag-of-Denmark.png" alt="">
-<img src="img/flags/flag-of-China.png" alt="">
-<img src="img/flags/flag-of-Chile.png" alt="">
+<img src="{{asset('img/flags/flag-of-United-States-of-America.png')}}" alt="">
+<img src="{{asset('img/flags/flag-of-United-Kingdom.png')}}" alt="">
+<img src="{{asset('img/flags/flag-of-Canada.png')}}" alt="">
+<img src="{{asset('img/flags/flag-of-Australia.png')}}" alt="">
+<img src="{{asset('img/flags/flag-of-Spain.png')}}" alt="">
+<img src="{{asset('img/flags/flag-of-Italy.png')}}" alt="">
+<img src="{{asset('img/flags/flag-of-Germany.png')}}" alt="">
+<img src="{{asset('img/flags/flag-of-France.png')}}" alt="">
+<img src="{{asset('img/flags/flag-of-Brazil.png')}}" alt="">
+<img src="{{asset('img/flags/flag-of-Venezuela.png')}}" alt="">
+<img src="{{asset('img/flags/flag-of-Sweden.png')}}" alt="">
+<img src="{{asset('img/flags/flag-of-Turkey.png')}}" alt="">
+<img src="{{asset('img/flags/flag-of-Russia.png')}}" alt="">
+<img src="{{asset('img/flags/flag-of-Portugal.png')}}" alt="">
+<img src="{{asset('img/flags/flag-of-New-Zealand.png')}}" alt="">
+<img src="{{asset('img/flags/flag-of-Netherlands.png')}}" alt="">
+<img src="{{asset('img/flags/flag-of-Malaysia.png')}}" alt="">
+<img src="{{asset('img/flags/flag-of-Liechtenstein.png')}}" alt="">
+<img src="{{asset('img/flags/flag-of-Lebanon.png')}}" alt="">
+<img src="{{asset('img/flags/flag-of-Japan.png')}}" alt="">
+<img src="{{asset('img/flags/flag-of-Jamaica.png')}}" alt="">
+<img src="{{asset('img/flags/flag-of-Ireland.png')}}" alt="">
+<img src="{{asset('img/flags/flag-of-India.png')}}" alt="">
+<img src="{{asset('img/flags/flag-of-Iceland.png')}}" alt="">
+<img src="{{asset('img/flags/flag-of-Hungary.png')}}" alt="">
+<img src="{{asset('img/flags/flag-of-Ghana.png')}}" alt="">
+<img src="{{asset('img/flags/flag-of-Greece.png')}}" alt="">
+<img src="{{asset('img/flags/flag-of-Cote-d-Ivoire.png')}}" alt="">
+<img src="{{asset('img/flags/flag-of-Estonia.png')}}" alt="">
+<img src="{{asset('img/flags/flag-of-Ecuador.png')}}" alt="">
+<img src="{{asset('img/flags/flag-of-Czech-Republic.png')}}" alt="">
+<img src="{{asset('img/flags/flag-of-Denmark.png')}}" alt="">
+<img src="{{asset('img/flags/flag-of-China.png')}}" alt="">
+<img src="{{asset('img/flags/flag-of-Chile.png')}}" alt="">
 </center>
 </div>
 
@@ -54,7 +54,7 @@ div.shabba img{
         	<li><a href="{{url('/')}}">Home</a></li>
             <li><a href="{{url('about')}}">About Us</a></li>
             <li><a href="{{url('apply')}}">Apply Now</a></li>
-            <li class="small-logo"><a href="{{url('/')}}"><img src="img/coat-of-arms.png" alt="" width="55" height="57"></a></li>
+            <li class="small-logo"><a href="{{url('/')}}"><img src="img/coat-of-arms.png')}}" alt="" width="55" height="57"></a></li>
             <li><a href="{{url('terms')}}">Terms</a></li>
             <li><a href="{{url('tales-of-joy')}}">Tales of Joy</a></li>
             <li><a href="{{url('contact')}}">Contact</a></li>
