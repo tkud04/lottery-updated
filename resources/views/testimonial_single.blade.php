53585e4458f6fb460ff59f205691e2d528e7a43b
Richmama <postmaster.richmama@gmail.com>
2017-11-04 09:32:25 +0100

    Changes in testimonial single

--- resources/views/testimonial_single.blade.php
+++ resources/views/testimonial_single.blade.php
@@ -1,19 +1,20 @@
 @extends('layout') 
 
-@section('title', $tales['title'])
-
-@section('content')
-<section class="main-section" id="service"><!--main-section-start-->
-	<div class="container">
-		<?php 
+<?php 
           $img = "img/".$tales['img'];
           $url = url("tales-of-joy")."/".$tales['url'];
           $name = $tales['name'];
           $title = $tales['title'];
           $content = $tales['content'];
-        ?>
+   ?>
+        	
+@section('title', $title)
+
+@section('content')
+<section class="main-section" id="service"><!--main-section-start-->
+	<div class="container">
     	<h2>Congratulations, {{$name}}! </h2>
-    	<h3>$1,000,000.00</h6>
+    	<h3>$1,000,000.00</h3>
         <div class="row">
         	<div class="col-lg-4 col-sm-6  wow fadeInUp delay-02s">
             	<img src="{{$img}}" alt=""><br>
