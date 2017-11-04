155f60cf06313653085109e25cbc505b53093872
Richmama <postmaster.richmama@gmail.com>
2017-11-04 12:15:02 +0100

    Added asset

--- resources/views/footer.blade.php
+++ resources/views/footer.blade.php
@@ -1,6 +1,6 @@
 <footer class="footer">
     <div class="container">
-        <div class="footer-logo"><a href="#"><img src="img/coat-of-arms.png" alt=""></a></div>
+        <div class="footer-logo"><a href="#"><img src="{{asset('img/coat-of-arms.png')}}" alt=""></a></div>
         <span class="copyright">&copy; WorldLotteryUSA. All Rights Reserved</span>
         <div class="credits">
             <a href="{{url('/')}}">Brought to you</a> by <a href="{{url('/')}}">WorldLotteryUSA</a>
