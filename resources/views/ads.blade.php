f7bc683dba0eb69699dbef3f069219e516f0c9a7
tkud04 <32499735+tkud04@users.noreply.github.com>
2017-11-07 07:06:30 +0100

    Added ads view via web

---
+++ resources/views/ads.blade.php
@@ -0,0 +1,9 @@
+<section class="main-section" id="service"><!--main-section-start-->
+	<div class="container">
+    	<div class="row">
+        	<div class="col-lg-12">
+            	<img id="ad-active" src="{{asset('img/ad-1.jpg')}}" alt="" class="img img-responsive">
+            </div>
+        </div>
+    </div>
+</section><!--main-section-end-->
