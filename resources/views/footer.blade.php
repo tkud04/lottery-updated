467c4cbc790bb372e80d9fe3e84b1247bf413269
Richmama <postmaster.richmama@gmail.com>
2017-11-08 11:32:15 +0100

    Added verified to footer

--- resources/views/footer.blade.php
+++ resources/views/footer.blade.php
@@ -1,5 +1,6 @@
 <footer class="footer">
     <div class="container">
+    	<img src="{{asset('img/verified.jpg')}}" alt="">
         <div class="footer-logo"><a href="#"><img src="{{asset('img/coat-of-arms.png')}}" alt=""></a></div>
         <span class="copyright">&copy; WorldLotteryUSA. All Rights Reserved</span>
         <div class="credits">
