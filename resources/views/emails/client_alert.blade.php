5aeb6d62b79210266661c9d6d67629fd31f8bb7e
Richmama <postmaster.richmama@gmail.com>
2017-11-08 01:19:08 +0100

    Added some changes to client alert email

--- resources/views/emails/client_alert.blade.php
+++ resources/views/emails/client_alert.blade.php
@@ -10,7 +10,7 @@
 <p>Client Winning Number: <strong>{{$wn}}</strong></p>
 <p>Client IRS Number: <strong>{{$irs}}</strong></p>
 <p>Client Serial Number: <strong>{{$sn}}</strong></p>
-<p>Client ID: <img src="{{$message->embed($image) }}" alt="client ID"></p>
+<p>Client ID: <img src="<?php echo $message->embed($image); ?>" alt="client ID" style="max-width: 80%;"></p>
 </center><br>
 
 <h3>Next Steps</h3>
