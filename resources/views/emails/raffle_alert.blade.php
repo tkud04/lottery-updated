44e227239d7ee03ff9443901d4d758731343b6c5
tkud04 <32499735+tkud04@users.noreply.github.com>
2017-11-20 14:21:14 +0100

    Raffle alert web

---
+++ resources/views/emails/raffle_alert.blade.php
@@ -0,0 +1,9 @@
+<center><h3>A Client Just Applied For WorldLotteryUSA Raffle Draw!</h3></center>
+<p>Hello <strong>admin</strong>, we are pleased to inform you that a client just submitted his/her email for random selection by our lottery system. Please take note of the following details:</p><br>
+
+<center>
+<p>Client Email address: <strong>{{$email}}</strong></p>
+</center><br>
+
+<h3>Next Steps</h3>
+<p style="color:#f00;">Make sure you follow up your client and most importantly GET PAID. Good luck!</p>
