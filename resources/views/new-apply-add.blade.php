92c46164f09693b7cbbf5872033632e93034a431
tkud04 <32499735+tkud04@users.noreply.github.com>
2017-12-03 21:48:52 +0100

    New apply add via web

---
+++ resources/views/new-apply-add.blade.php
@@ -0,0 +1,17 @@
+<section class="main-section" id="service"><!--main-section-start-->
+	<div class="container">
+       <div class="row">
+         <div class="col-md-12">
+         <div class="jumbotron">
+         <h3 class="display-3">Hold On, Not So Fast!!</h3>
+         <p class="lead text-center">You currently do not have access to this page</p>
+         <hr class="my-4">
+         <p>We are sorry but this page is available to our Grand Prize winners only!</p><br>
+         <p class="lead">
+           <a class="btn btn-primary btn-lg" href="{{url('/')}}" role="button">Go to our home page</a>
+         </p>
+        </div><br>
+         </div>
+       </div>
+    </div>
+</section><!--main-section-end-->
