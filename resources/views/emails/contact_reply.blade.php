0128ac6d1b9f34b9a6a93138b3390bf1a68e53ff
tkud04 <32499735+tkud04@users.noreply.github.com>
2017-12-29 16:17:52 +0100

    Contact reply via web

---
+++ resources/views/emails/contact_reply.blade.php
@@ -0,0 +1,14 @@
+<center><h3>{{$subject}}</h3></center>
+<p>Hello, <strong>{{$name}}</strong>! We appreciate you contacting us and we want to assure we aim to address your issues and concerns!</p><br>
+
+<center>
+<blockquote>
+{{$message}}
+</blockquote>
+
+<h3>Our Response</h3>
+{{$response}}
+
+<br><br>
+<p>Thanks you for contacting us, and we hope your issues or concerns have been addressed.</p><br>
+<p>WorldLotteryUSA Inc.</p>
