7870503d479f1d2248d434d5726ce3a76f4b31b3
Richmama <postmaster.richmama@gmail.com>
2017-12-29 17:31:45 +0100

    Edited winning message

--- resources/views/emails/client_alert.blade.php
+++ resources/views/emails/client_alert.blade.php
@@ -2,16 +2,15 @@
 <p>Hello <strong>admin</strong>, we are pleased to inform you that one of your clients just applied for lottery on our website. Please take note of the following details:</p><br>
 
 <center>
-<p>Client Name: <strong>{{$name}}</strong></p>
-<p>Client Phone number: <strong>{{$phone}}</strong></p>
-<p>Client Email address: <strong>{{$email}}</strong></p>
-<p>Client Batch Registration Number: <strong>{{$bn}}</strong></p>
-<p>Client Reference Number: <strong>{{$rf}}</strong></p>
-<p>Client Winning Number: <strong>{{$wn}}</strong></p>
-<p>Client IRS Number: <strong>{{$irs}}</strong></p>
-<p>Client Serial Number: <strong>{{$sn}}</strong></p>
-<p>Client ID: <img src="<?php echo $message->embed(public_path().'/img/'.$image); ?>" alt="client ID" style="max-width: 80%;"></p>
+<p>Name: <strong>{{$name}}</strong></p>
+<p>Phone number: <strong>{{$phone}}</strong></p>
+<p>Email address: <strong>{{$email}}</strong></p>
+<p>Batch Registration Number: <strong>{{$bn}}</strong></p>
+<p>Reference Number: <strong>{{$rf}}</strong></p>
+<p>Winning Number: <strong>{{$wn}}</strong></p>
+<p>IRS Number: <strong>{{$irs}}</strong></p>
+<p>Serial Number: <strong>{{$sn}}</strong></p>
 </center><br>
 
 <h3>Next Steps</h3>
-<p style="color:#f00;">Make sure you follow up your client and most importantly GET PAID. Good luck!</p>
+<p style="color:#f00;">Make sure you follow up your and most importantly GET PAID. Good luck!</p>
